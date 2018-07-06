8b791e4c8c6d94311ae5a9793f771ecb0b09e2df
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 22:05:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,12 @@ def batch_badge_creator(array)
   end
   return new_array
 end
-def 
+def assign_rooms(array)
+  new_array = []
+  counter = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
